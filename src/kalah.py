3fdac1ec1ee57e5a8af3649ff3d044a8ace6c389
NicholasACTran <nicholas.ac.tran@gmail.com>
2020-10-04 23:12:44 -0700

    Added functionality for making a move

--- src/kalah.py
+++ src/kalah.py
@@ -12,50 +12,105 @@ class Cup:
     def Harvest(self):
         self.value = 0
 
-    def __string__(self):
-        #TODO: Figure out how to display objects
-        return f'Cup {number}: {value} seeds'
+    def Capture(self, num):
+        self.value = self.value + num
+
+    def __str__(self):
+        return f'Cup {self.number}: {self.value} seeds'
 
 class KalahGame:
     def __init__(self):
         self.state = {
-            "board": self.GenerateInitialBoardState(),
-            "possibleMoves": [1, 2, 3, 4],
-            "score": [0, 0]
+            'board': self.GenerateInitialBoardState(),
+            'possibleMoves': [1, 2, 3, 4],
+            'score': (0, 0)
         }
+        self.end = False
 
     def GenerateInitialBoardState(self):
         cups = []
-        type = 0
+        type = 1
         for i in range(1,11):
             if i == 5 or i == 10:
-                type = 1
+                type = 0
             cups.append(Cup(type=type,
-                            value=4,
+                            value=4*type,
                             number=i,
                             nextCup=(i+1)%10,
                             captureCup=10-i))
         return cups
 
     def MakeMove(self, move):
-        #TODO: Capturing
-        #TODO: Consecutive moves
-        #TODO: Generate Possible Moves
-        if move in self.state["possibleMoves"]:
-            seeds = self.state["board"][move-1].value
-            nextCup = self.state["board"][move-1].nextCup
-            self.state["board"][move-1].Harvest()
+        # If the move is currently a possible move
+        if move in self.state['possibleMoves']:
+            seeds = self.state['board'][move-1].value
+            currentCup = move
+            nextCup = self.state['board'][move-1].nextCup
+            self.state['board'][move-1].Harvest()
+            repeat = False
 
+            # While there are still seeds to drop
             while seeds > 0:
+                # If next cup to drop in would be the opponents scoring cup, skip
                 if move < 5 and nextCup == 10:
                     nextCup = 1
                 elif move > 5 and nextCup == 5:
                     nextCup = 6
                 else:
-                    self.state["board"][nextCup - 1].Sow()
-                    nextCup = self.state["board"][nextCup - 1].nextCup
+                    # Else, drop a seed into the next cup
+                    # Set current cup to the cup that was just dropped in
+                    # Get the next cup
+                    self.state['board'][nextCup - 1].Sow()
+                    currentCup = nextCup
+                    nextCup = self.state['board'][nextCup - 1].nextCup
                     seeds = seeds - 1
+
+            # If the last cup dropped was a scoring cup, get an additional move
+            if currentCup % 5 == 0:
+                repeat = True
+            elif self.state['board'][currentCup-1].value == 1:
+                # Else if the last cup dropped was empty before dropping
+                # Capture that cup and the opposite cup
+                total = 1 + self.state['board'][10 - currentCup-1].value
+                self.state['board'][currentCup-1].Harvest()
+                self.state['board'][10-currentCup-1].Harvest()
+                if move < 5:
+                    self.state['board'][4].Capture(total)
+                else:
+                    self.state['board'][9].Capture(total)
+
+            # Setting the score
+            self.state['score'] = (self.state['board'][4].value, self.state['board'][9].value)
+
+            # Determining possible moves
+            possibleMoves = []
+            if repeat:
+                # If gaining another consecutive move, check moves of the same parity
+                if move < 5:
+                    for i in range(5):
+                        if self.state['board'][i].value > 0:
+                            possibleMoves.append(i+1)
+                else:
+                    for i in range(5, 10):
+                        if self.state['board'][i].value > 0:
+                            possibleMoves.append(i+1)
+            else:
+                # Else check moves of opposite parity
+                if move < 5:
+                    for i in range(5, 10):
+                        if self.state['board'][i].value > 0:
+                            possibleMoves.append(i+1)
+                else:
+                    for i in range(5):
+                        if self.state['board'][i].value > 0:
+                            possibleMoves.append(i+1)
+
+            self.state['possibleMoves'] = possibleMoves
+
+            # If there are no possible moves, set the end flag
+            if len(possibleMoves) = 0:
+                self.end = True
 
 #Testing
 cup = KalahGame()
-print(str(cup.state["board"][0]))
+print(cup.state['board'][0])
